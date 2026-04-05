5543affdac6316e7b4b4ee085fe0c56d09b01710
meghana-220585 <n220585@rguktn.ac.in>
2026-04-05 20:18:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
-function addStudent() {
-    let name = document.getElementById("name").value;
-    let marks = document.getElementById("marks").value;
-
-    if(name === "" || marks === "") {
-        alert("Please enter all details");
-        return;
-    }
-
-    let list = document.getElementById("list");
-
-    let li = document.createElement("li");
-    li.innerText = name + " - Marks: " + marks;
-
-    list.appendChild(li);
-
-    // clear inputs
-    document.getElementById("name").value = "";
-    document.getElementById("marks").value = "";
-}
+// Order button alert
+document.querySelectorAll(".btn-warning").forEach(btn => {
+    btn.addEventListener("click", () => {
+        alert("Order placed successfully!");
+    });
+});
